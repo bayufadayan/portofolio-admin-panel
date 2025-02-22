85c04e640ab26e1e8d8361123c0acf2b16632260
bayufadayan <bayufadayan@gmail.com>
2025-02-22 11:49:39 +0700

    fix: sidebar toggle feature

--- src/components/cardCount.tsx
+++ src/components/cardCount.tsx
@@ -9,8 +9,9 @@ interface CardCountProps {
 
 const CardCount: React.FC<CardCountProps> = ({ title, value, color, icon }) => {
     return (
-        <div className={`gap-3 p-5 bg-gradient-to-tr ${color} rounded-lg shadow-md flex items-center justify-between border-l-4 border-${color}-600 hover:-translate-y-1 hover:shadow-lg transition-transform duration-300 ease-in-out hover:cursor-pointer`}
-            style={{ width: 'calc(33.333% - 32px)' }}>
+        <div
+            className={`gap-3 p-5 bg-gradient-to-tr ${color} rounded-lg shadow-md flex items-center justify-between border-l-4 border-${color}-600 hover:-translate-y-1 hover:shadow-lg transition-transform duration-300 ease-in-out hover:cursor-pointer w-full sm:w-[calc(50%-32px)] md:w-[calc(33.333%-32px)]`}
+        >
             <div className='flex flex-col justify-between'>
                 <h2 className={`text-md font-bold mb-2 text-white`}>
                     {title}
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,26 +1,79 @@
-import Link from 'next/link'
-import React from 'react'
+'use client'
+import Link from 'next/link';
+import React, { useState } from 'react';
 
 export default function SideBar() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
     return (
         <div>
-            <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+            <button
+                data-drawer-target="default-sidebar"
+                data-drawer-toggle="default-sidebar"
+                aria-controls="default-sidebar"
+                type="button"
+                onClick={toggleSidebar}
+                className={`inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600`}>
                 <span className="sr-only">Open sidebar</span>
-                <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                {isSidebarOpen ? (<svg
+                    className="w-6 h-6"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        clipRule="evenodd"
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    ></path>
+                </svg>) : (<svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-                </svg>
+                </svg>)}
             </button>
 
-            <aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+            <aside
+                id="default-sidebar"
+                className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
+                    } sm:translate-x-0`}
+                aria-label="Sidebar">
                 <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-                    <div className='my-4 text-center'>
+                    <div className='my-0 md:my-4 text-right md:text-center'>
+                        <button
+                            data-drawer-target="default-sidebar"
+                            data-drawer-toggle="default-sidebar"
+                            aria-controls="default-sidebar"
+                            type="button"
+                            onClick={toggleSidebar}
+                            className={`inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600`}>
+                            <span className="sr-only">Open sidebar</span>
+                            {isSidebarOpen ? (<svg
+                                className="w-6 h-6"
+                                aria-hidden="true"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    clipRule="evenodd"
+                                    fillRule="evenodd"
+                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                ></path>
+                            </svg>) : (<svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+                            </svg>)}
+                        </button>
                         <Link href={"/"}>
                             <div className='w-full bg-red-500 text-white align-middle mb-2 p-2 font-bold text-xl text-center rounded-lg'>
                                 <p>PORTOFOLIO</p>
                                 <p>ADMIN PANEL</p>
                             </div>
                         </Link>
-                        <p>Go to <Link href={'http://bayufadayan.my.id'} className='text-blue-400 font-semibold hover:underline hover:text-blue-500'>bayufadayan.my.id</Link></p>
+                        <p className='text-left'>Go to <Link href={'http://bayufadayan.my.id'} className='text-blue-400 font-semibold hover:underline hover:text-blue-500'>bayufadayan.my.id</Link></p>
                     </div>
 
                     <ul className="pt-4 mt-4 space-y-2 font-medium border-t border-gray-200 dark:border-gray-700">
